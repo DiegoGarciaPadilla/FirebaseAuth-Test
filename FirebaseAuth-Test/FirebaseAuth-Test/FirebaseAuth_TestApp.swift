52cd3c30653d41500105f13ff004fb174a431d32
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-10-06 12:11:31 -0600

    feat: initialize xcode project

---
+++ FirebaseAuth-Test/FirebaseAuth-Test/FirebaseAuth_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuth_TestApp.swift
+//  FirebaseAuth-Test
+//
+//  Created by Diego Antonio García Padilla on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuth_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
